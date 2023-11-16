1803d83b11ec0c57f32fb5df0a348d7090f7e918
marius <mebc@gmx.net>
2023-11-16 01:25:37 +0100

    path adapted

--- corner_detect.sh
+++ corner_detect.sh
@@ -2,8 +2,9 @@
 # This Program is started from main2ext.py to find corner coordinates from image 000image.png
 echo off
 clear
-cd ~/Desktop/pyprojects/csr-jet/chess_state_recognition
+cd chess_state_recognition
 # script to start chess board corner detector
+
 echo ---------------------------------------------------------
 echo RUNNING ...
 echo PLEASE WAIT 10 secs FOR RESULT
@@ -11,6 +12,6 @@ echo ---------------------------------------------------------
 
 ## cd Desktop/pyprojects/csr/chess_state_recognition
 # python3 main.py examples/small_test/0046.png .
-python3 main_test.py /home/marius/Desktop/pyprojects/ChessGameTracking/000image.png .
+python3 main_test.py ../000image.png .
 # do not delete the dot in line 13
 
